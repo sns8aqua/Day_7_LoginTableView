036a606f23e915e1fb0f3f2ca7b9b9f8143504a0
SathyaNarayan <roflmeatu@gmail.com>
2022-01-24 23:41:30 +0100

    Login view

---
+++ LoginTableView/LoginTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LoginTableViewCell.swift
+//  LoginTableView
+//
+//  Created by Santhosh on 17/01/22.
+//
+
+import UIKit
+
+class LoginTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- LoginTableView/ViewController.swift
+++ LoginTableView/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myTable.delegate = self
+        myTable.dataSource = self
     }
 
 
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LoginTableViewCellIdentifier", for: indexPath)
+        if let cellIs = cell as? LoginTableViewCell {
+           
+        }
+        return cell
+    }
+
+}
